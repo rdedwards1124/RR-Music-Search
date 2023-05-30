175880d67bb8d316bb2a918292e179abc12d9a16
Rashonda Edwards <rashondaedwards@aol.com>
2023-05-29 20:24:20 -0700

    commit_5-29

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import {useEffect, useState } from 'react'
+import {Fragment, useEffect, useState } from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import { DataContext } from './context/DataContext'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
 	let [search, setSearch] = useState('')
@@ -33,11 +36,21 @@ function App() {
 
 	return (
 		<div>
-			<SearchBar handleSearch = {handleSearch}/>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+      {message}
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <Fragment>
+              <SearchBar handleSearch = {handleSearch}/>
+              <DataContext.Provider value={data}>
+                <Gallery />
+              </DataContext.Provider>
+            </Fragment>
+          } />
+          <Route path="/album/:id" element={<AlbumView/>} />
+          <Route path="/artist/:id" element={<ArtistView/>} />
+        </Routes>
+      </Router>
 		</div>
   	);
 }
